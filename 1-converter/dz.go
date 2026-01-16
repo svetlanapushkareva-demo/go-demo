22f5c769359082eb0a13e438477e9ff0e9500382
Svetlana Pushkareva <pushkarevasvetlana2003@gmail.com>
2026-01-16 20:06:45 +0300

    Revert "пересдача домашнего задания"
    This reverts commit 867fd3e06f2754f248e3906e73d1b22ce84c91d6.

--- 1-converter/dz.go
+++ 1-converter/dz.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	const USD2EUR = 1.16
-	const USD2RUB = 77.75
-	const EUR2RUB = USD2EUR * USD2RUB / 100
-	var EURMoney float64 = 100
+	const USD2EUR = 2
+	const USD2RUB = 100
+
+	var EURMoney float64 = 1000
 
 	fmt.Println(EURMoney, "изначальные евро")
 
@@ -17,5 +17,4 @@ func main() {
 	RUBMoney := USDMoney * USD2RUB
 
 	fmt.Println(RUBMoney, "рубли")
-	fmt.Println(EUR2RUB, "евро в рублях")
 }
